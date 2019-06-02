b952e48d446ec28685e5c469fe829dc3550af971
Victor Scherer Putrich <scherer.victor98@gmail.com>
2019-06-02 16:55:33 -0300

    Soluc. força_bruta
    comentado uma tentativa de fazer com programação dinâmica: tentei usar uma espécie de código hash gerado a partir da soma dos valores de cada objeto, porém existem objetos de mesmo valor com pesos diferentes. Minha solução seria pensa numa maneira de gerar o hash levando em consideração o peso de cada objeto também.
    Solução do Hash: para gerar o vetor que guarda as somas seria a 2^(número de objetos), assim, existe apenas uma maneira de encontrar uma posição no vetor que seria somar os mesmo objetos em ordens diferentes.

--- prob_mochila.py
+++ prob_mochila.py
@@ -20,13 +20,31 @@ class Mochila:
     def __init__(self, peso_maximo):
         self.peso_maximo = peso_maximo
         self.maior_valor = 0
+        self.peso_usado = 0
         #self.vetor_quebra_recursao = []
-    def coloca_coisas(self, vet, valor_atual, peso_atual):
+    def coloca_coisas(self, vet, peso_atual, valor_soma):
+        if len(vet) == 0 or peso_atual > self.peso_maximo:
+            return    
+        elif valor_soma > self.maior_valor and peso_atual <= self.peso_maximo:
+            self.peso_usado = peso_atual
+            self.maior_valor = valor_soma
+        for i in range(len(vet)):
+            peso = peso_atual + vet[i].get_peso()
+            soma = valor_soma + vet[i].get_valor()
+            new_vetor = vet.copy()
+            del(new_vetor[i])
+            self.coloca_coisas(new_vetor, peso, soma)    
+
+    def print_resultados(self):
+        print(self.peso_usado)
+        print(self.maior_valor)
+        '''
+        print(valor_atual)
         if len(vet) == 0 or peso_atual > self.peso_maximo:
             return 
         if valor_atual > self.maior_valor:
             self.maior_valor = valor_atual
-        
+            self.peso_usado = peso_atual
         for i in range(len(vet)):
             objeto = vet[i]
             peso_atual += objeto.get_peso()
@@ -34,8 +52,11 @@ class Mochila:
             vetr = vet.copy()
             vetr.remove(vet[i])
             self.coloca_coisas(vetr, valor_atual, peso_atual)
-        
+    def get_peso(self):
+        return self.peso_usado
+    def get_valor(self):
         return self.maior_valor
+    '''
 '''
 
     def define_mochila(self, vetor_objetos):
@@ -71,15 +92,17 @@ class Mochila:
 '''
     
     
-obj1 = Objetos(3,5)
-obj2 = Objetos(1,3)
-obj3 = Objetos(6,2)
-obj4 = Objetos(2,4)
-obj5 = Objetos(1,3)
-obj6 = Objetos(4,5)
+obj1 = Objetos(5,5)
+obj2 = Objetos(10,10)
+obj3 = Objetos(30,15)
+obj4 = Objetos(15,30)
+obj5 = Objetos(40,15)
+obj6 = Objetos(7,7)
 vetor_objetos = [obj1, obj2, obj3, obj4, obj5, obj6]
-mochila = Mochila(5)
+mochila = Mochila(15)
 #mochila.gera_vetor_hash(vetor_objetos)
 #mochila.define_mochila(vetor_objetos)
 mochila.coloca_coisas(vetor_objetos, 0, 0)
-                            
+#print(mochila.get_peso())
+#print(mochila.get_valor())
+mochila.print_resultados()                            
